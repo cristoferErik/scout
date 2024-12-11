1ea74242319ab2e6cce3e075bb30fac5b4922a00
Cristofer <cristofermil@gmail.com>
2024-12-11 18:22:33 +0100

    logo-body

---
+++ src/main/java/com/checker/scout/Body.java
@@ -0,0 +1,30 @@
+package com.checker.scout;
+
+public class Body {
+    public static final String bodyHtml="""
+        <div>
+        <br><br><br>
+            <h3>SEGUI IL NOSTRO CANALE DEDICATO A MARKETING E SVILUPPO WEB</h3>
+    
+            <p><strong>Davide Mangione</strong><br>
+            Comunicazione e sviluppo web</p>
+            
+            <p><strong>Telefono:</strong> +39 333 65 65 629</p>
+            
+            <p><strong>P. IVA:</strong> 08122050969<br>
+            <strong>SDI:</strong> M5UXCR1</p>
+            
+            <p><strong>Canali:</strong></p>
+            <ul>
+                <li><a href="https://youtube.com/@davide.spaceagency" target="_blank">youtube.com/@davide.spaceagency</a></li>
+                <li><a href="https://www.spaceagency.pro" target="_blank">www.spaceagency.pro</a></li>
+                <li><a href="https://www.davidemangione.it" target="_blank">www.davidemangione.it</a></li>
+            </ul>
+            
+            <p><strong>&lt;&lt; <a href="#portfolio">PORTFOLIO WEB</a> &gt;&gt;</strong></p>
+            <p><strong>&lt;&lt; <a href="#demo-video">DEMO VIDEO</a> &gt;&gt;</strong></p>
+             <img src="cid:logo" alt="Descrizione dell'imagine" />
+        </div>
+
+    """;
+}
--- src/main/java/com/checker/scout/services/EmailService.java
+++ src/main/java/com/checker/scout/services/EmailService.java
@@ -4,15 +4,16 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import static com.checker.scout.Body.bodyHtml;
 import com.checker.scout.controllers.home.interfaces.HomeInt;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-
 @Service
 public class EmailService {
 
@@ -36,11 +37,12 @@ public class EmailService {
                 }
 
                 helper.setTo(email);
-                helper.setText("<p>" + webSiteMessage.getMessage() + "</p>", true);
+                helper.setText("<div>" + webSiteMessage.getMessage() + bodyHtml +"</div>", true);
+                helper.addInline("logo", new ClassPathResource("static/images/space-agency/SPACE-AGENCY-LOGO.png"));
                 mailSender.send(message);
 
             } catch (MessagingException e) {
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
         });
 
